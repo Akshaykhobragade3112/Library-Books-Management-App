a47a004c8127017e07a140b0581061d836de9981
Akshay Khobragade <113614597+Akshaykhobragade3112@users.noreply.github.com>
2024-09-20 00:54:26 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,18 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config(); // Load .env variables
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (err) {
-    console.error(`Error: ${err.message}`);
-    process.exit(1);
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1); // Exit process with failure
   }
 };
 
